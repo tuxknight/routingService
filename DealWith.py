4dc5be58c53ce72916a48c063429203e9c0fdc6c
Biao.Zhao <zbhdsfdx@163.com>
2015-07-30 13:54:52 +0800

    add DealWith.py to input or output data

---
+++ DealWith.py
@@ -0,0 +1,36 @@
+
+class BaseDealWith:
+
+    def __init__(self, style):
+       print ' '
+
+    def in(self):
+       print ' '
+   
+    def to_service(self):
+       print ' '
+   
+    def from_service(self):
+       print ' '
+   
+    def out(self):
+       print ' '
+
+class DealWithFile(BaseDealWith):
+
+    def __init__(self,style):
+
+    def in(self):
+
+    def to_service(self):
+
+    def from_service(self):
+
+    def out(self):
+
+
+class DealWithTCP(BaseDealWith):
+
+class DealWithHttp(BaseDealWith):
+
+class DealWithUDP(BaseDealWith):
